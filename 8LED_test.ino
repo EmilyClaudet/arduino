d5ff6b2f3629655a41d1b22afde6d18ef29813ca
EmilyClaudet <emily@kano.me>
2015-12-27 12:28:50 -0500

    arduino project

---
+++ 8LED_test.ino
@@ -0,0 +1,47 @@
+
+int ledPins[] = {2,3,4,5,6,7,8,9};
+// setting array for digital outputs
+
+void setup()
+{
+  int index;
+   
+   for(index = 0; index <= 7; index++)
+  {
+    pinMode(ledPins[index],OUTPUT);
+    // ledPins[index] is replaced by the value in the array.
+    // For example, ledPins[0] is 2
+  }  
+}
+
+void loop() {
+
+    pingPong();             // Light the LEDs middle to the edges
+
+}
+
+void pingPong()
+{
+  int index;
+  int delayTime = 500; // milliseconds to pause between LEDs
+                       // make this smaller for faster switching
+  
+  // step through the LEDs, from 0 to 7
+  
+  for(index = 0; index <= 7; index = index + 2)
+  {
+    digitalWrite(ledPins[index], HIGH);  // turn LED on
+    delay(delayTime);                    // pause to slow down
+    digitalWrite(ledPins[index], LOW);   // turn LED off
+  }
+
+  // step through the LEDs, from 7 to 0
+  
+  for(index = 7; index >= 0; index = index - 2)
+  {
+    digitalWrite(ledPins[index], HIGH);  // turn LED on
+    delay(delayTime);                    // pause to slow down
+    digitalWrite(ledPins[index], LOW);   // turn LED off
+  }
+}
+
